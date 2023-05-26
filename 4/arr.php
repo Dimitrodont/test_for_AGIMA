1ac020ac625786f58ea72a0fb9d60aae374f754e
Dimitrodont <132064523+Dimitrodont@users.noreply.github.com>
2023-05-26 19:34:26 +0300

    Update arr.php

--- 4/arr.php
+++ 4/arr.php
@@ -1,29 +1,16 @@
 <?php
-
     $arr = ["Masha" => "manager", "Sasha" => "director", "Misha" => "designer", "Dima" => "linguist"];
-
     $keys = [];
-    
-$keys_length = 0;
-
+    $keys_length = 0;
 
     echo "direct pass\n";
-    
-foreach($arr as $key => $val){
-
+    foreach($arr as $key => $val){
         echo $key . " => " . $val . "\n";
-
         $keys[] = $key;
-
     }
-
     $keys_length = count($keys);
-
     
-echo "reverse pass\n";
-    
-for($i = $keys_length - 1; $i >= 0; --$i){
-
+    echo "reverse pass\n";
+    for($i = $keys_length - 1; $i >= 0; --$i){
         echo $keys[$i] . " => " . $arr[$keys[$i]] . "\n";
-
     }
